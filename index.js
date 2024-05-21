5ae8b090f073480f7e4fa75bce131727475ac7fe
HasnatShahriar <shahriarhriday94@gmail.com>
2024-05-22 02:00:09 +0700

    'post

--- index.js
+++ index.js
@@ -29,21 +29,35 @@ async function run() {
 
     const menuCollection = client.db('bistroDb').collection('menu');
     const reviewCollection = client.db('bistroDb').collection('reviews');
+    const cartCollection = client.db('bistroDb').collection('carts');
 
     // get data for menu 
-    app.get('/menu', async(req,res)=> {
+    app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result)
     })
 
     // get data for reviews
-    app.get('/reviews', async(req,res)=> {
+    app.get('/reviews', async (req, res) => {
       const result = await reviewCollection.find().toArray();
       res.send(result)
     })
 
 
+    // carts collection
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      const query = {email: email};
+      const result = await cartCollection.find(query).toArray();
+      res.send(result)
+    })
 
+
+    app.post('/carts', async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result)
+    })
 
 
 
